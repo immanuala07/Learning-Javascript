b33ed5543af94a287b51657b0c12dcc5e3677698
Immanual <immanualatrinity@gmail.com>
2021-08-28 12:45:29 +0530

    updating jsdoc

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@
 const debounce = (fn, delay) => {
 	let timeoutID;
 
-	// Higher order function
+	// Higher order function which passes ...args which is PointerEvent
 	return function (...args) {
 
+		// If the timeoutID exist then clear the timeout and set the timeout again in the below code
 		if (timeoutID) {
 			clearTimeout(timeoutID);
 		}
 
+		// setTimeout based on the delay passed on the function and call the  event handler function by passing the PointerEvent
 		timeoutID = setTimeout(() => {
 			fn(...args)
 		}, delay);
